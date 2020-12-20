0eb7f1d813acd6a4db0f749c90f9eae7d18212e1
dlhardi <hardisonderrick@gmail.com>
2020-12-20 03:37:59 -0500

    added my own API key for open weather map. added ajax methods and query URLs for current weather and five day forecast using the name of a city as the search parameter. name of city currently hardcoded

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
 $(document).ready(function () {
+  //DOM VARIABLES
 
+  //JAVASCRIPT VARIABLES
+  var nameOfCity = "atlanta";
+  var APIKey = "7138f209713bfc5af60e3ab4b13ebf4e";
+  // Here we are building the URL we need to query the database
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    nameOfCity +
+    "&appid=" +
+    APIKey;
 
+    var queryURL2 =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    nameOfCity +
+    "&appid=" +
+    APIKey;
 
-    function searchForACity (){
-         console.log("new test");
-     }
-    
-
-    // Event listeners
-
-    // adds event listener to search button
-
-    $(".search-button").on("click", function(){
-        searchForACity();
+  //FUNCTION DEFINITIONS
+  function searchForACity() {
+    // Here we run our AJAX call to the OpenWeatherMap API
+    $.ajax({
+      url: queryURL,
+      method: "GET",
     })
-    
-    });
+      // We store all of the retrieved data inside of an object called "response"
+      .then(function (response) {
+        // Log the queryURL
+        console.log(queryURL);
+
+        // Log the resulting object
+        console.log(response);
+      });
+
+      $.ajax({
+          url: queryURL2,
+          method: "GET",
+      }).then(function(response){
+        console.log(queryURL2);
+        console.log(response)
+      });
+  }
+
+  // FUNCTION CALLS
+
+  // Event listeners
+  $(".search-button").on("click", function () {
+    searchForACity();
+  });
+});
